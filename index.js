c4947f63cf65a1b6f921b757aa7dbae6db6c7ef5
stuart runyan <shrunyan@gmail.com>
2018-08-27 16:53:19 -0700

    IRL code review

--- index.js
+++ index.js
@@ -1,77 +1,108 @@
-const request = require('request')
-const url = require('url')
+const request = require("request");
+const url = require("url");
+const http = require("http");
+const crypto = require("crypto");
 
 let opts = {
-  githubClient: process.env['GITHUB_CLIENT'],
-  githubSecret:  process.env['GITHUB_SECRET'],
-  baseURL: 'http://localhost',
-  loginURI: '/login',
-  callbackURI: '/callback',
-  scope: 'user' // optional, default scope is set to user
-}
+  githubClient: process.env["GITHUB_CLIENT"],
+  githubSecret: process.env["GITHUB_SECRET"],
+  baseURL: "http://localhost",
+  loginURI: "/login",
+  callbackURI: "/callback",
+  scope: "user" // optional, default scope is set to user
+};
 
-const crypto = require("crypto");
-const state = crypto.randomBytes(20).toString('hex');
-let port = 8080
-
-require('http').createServer(function(req, res) {
-  if (req.url.match(/login/)) return login(req, res)
-  if (req.url.match(/callback/)) return callback(req, res)
-    else return redirect(req, res, "/login")
-}).listen(port)
-
-console.log("Now listening on http://localhost:8080/")
+// Using a cryptographically signed string to pass through
+// the various steps within the OAuth flow to ensure
+// the request has not been tampered with.
+const sig = crypto.randomBytes(20).toString("hex");
+const port = 8080;
 
 function redirect(req, resp, destination) {
-resp.statusCode = 301
-  resp.setHeader('location', destination)
-  resp.end()
+  resp.statusCode = 301;
+  resp.setHeader("location", destination);
+  resp.end();
 }
+
 function login(req, resp) {
-	let u = 'https://github.com/login/oauth/authorize'
-	    + '?client_id=' + opts.githubClient
-	    + (opts.scope ? '&scope=' + opts.scope : '')
-	    + '&state=' + state
-	    ;
-	resp.statusCode = 302
-	resp.setHeader('location', u)
-	resp.end()
+  const state = {
+    reffer: req.url,
+    signature: sig
+  };
+
+  const url =
+    "https://github.com/login/oauth/authorize" +
+    "?client_id=" +
+    opts.githubClient +
+    (opts.scope ? "&scope=" + opts.scope : "") +
+    "&state=" +
+    JSON.stringify(state);
+
+  resp.statusCode = 302;
+  resp.setHeader("location", url);
+  resp.end();
 }
 
-function callback(req, resp, cb) {
-	var query = url.parse(req.url, true).query
-    var code = query.code
-    if (!code) return gotToken('missing oauth code', null, null, req, resp)
-    var u = 'https://github.com/login/oauth/access_token'
-       + '?client_id=' + opts.githubClient
-       + '&client_secret=' + opts.githubSecret
-       + '&code=' + code
-       + '&state=' + state
-       ;
-    request.get({url:u, json: true}, function (err, tokenResp, body) {
+function callback(req, resp) {
+  const query = url.parse(req.url, true).query;
+  const code = query.code;
+  if (!code) {
+    // Send 400 missing oauth code
+  }
+
+  const url =
+    "https://github.com/login/oauth/access_token" +
+    "?client_id=" +
+    opts.githubClient +
+    "&client_secret=" +
+    opts.githubSecret +
+    "&code=" +
+    code +
+    "&state=" +
+    state;
+
+  request.get(
+    {
+      url: url,
+      json: true
+    },
+    function(err, tokenResp, body) {
       if (err) {
-        if (cb) {
-          err.body = body
-          err.tokenResp = tokenResp
-          return cb(err)
-        }
-        return gotToken(err, body, tokenResp, req, resp)
+        // TODO send 500 response
+      } else {
+        const state = JSON.parse(query.state);
+
+        // TODO Store DB
+
+        resp.writeHead(200, { "Content-Type": "text/html" });
+
+        // Redirect user back to app
+        resp.setHeader("location", state.reffer);
       }
-      if (cb) {
-        cb(null, body)
-      }
-      gotToken(null, body, tokenResp, req, resp)
-    })
+    }
+  );
 }
 
-function gotToken(error, token, tokenResp, req, resp) {
-  if (!error) {
-    console.log(token.access_token)
-    resp.writeHead(200, {'Content-Type': 'text/html'})  
-    resp.write(`Access Token: <a href="https://api.github.com/user?access_token=${token.access_token}">${token.access_token}</a>`)
-    resp.end()
-  }
-  else {
-    console.log(error)
-  }
-}
+http
+  .createServer((req, res) => {
+    console.log(`URL: ${req.url}`);
+
+    if (req.url === "/") {
+      res.statusCode = 200;
+      res.write("alive");
+      res.end();
+      return;
+    } else if (req.url === "/login/") {
+      return login(req, res);
+    } else if (url.parse(req.url).pathname === "/callback/") {
+      return callback(req, res);
+    } else {
+      res.statusCode = 200;
+      res.write(`Request URL ${req.url} does not exist`);
+      res.end();
+      return;
+    }
+  })
+  .listen(port, () => {
+    console.log("Now listening on http://localhost:8080/");
+  });
